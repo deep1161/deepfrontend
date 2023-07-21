a53dcd107e1400a320cf126530cc7dce79a94042
deepjyoti <2020ucp1161@mnit.ac.in>
2023-07-21 11:19:20 +0530

    Initial commit

---
+++ src/components/Banner.tsx
@@ -0,0 +1,41 @@
+import { Carousel } from "react-responsive-carousel";
+import sliderImg_1 from "../images/slider/sliderImg_1.jpg";
+import sliderImg_2 from "../images/slider/sliderImg_2.jpg";
+import sliderImg_3 from "../images/slider/sliderImg_3.jpg";
+import prime from "../images/slider/prime.jpg";
+import fashion from "../images/slider/fashion.jpg";
+import Image from "next/image";
+
+const Banner = () => {
+  return (
+    <div className="relative">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={3000}
+      >
+        <div>
+          <Image priority src={prime} alt="sliderImg" />
+        </div>
+        <div>
+          <Image src={fashion} alt="sliderImg" />
+        </div>
+        <div>
+          <Image src={sliderImg_1} alt="sliderImg" />
+        </div>
+        <div>
+          <Image src={sliderImg_2} alt="sliderImg" />
+        </div>
+        <div>
+          <Image src={sliderImg_3} alt="sliderImg" />
+        </div>
+      </Carousel>
+      <div className="w-full h-40 bg-gradient-to-t from-gray-100 to-transparent absolute bottom-0 z-20"></div>
+    </div>
+  );
+};
+
+export default Banner;
